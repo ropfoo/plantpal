018af6c325615bacc199e2fe5a730b57c428b118
RopFoo <ropfoo@gmail.com>
2023-02-23 23:54:56 +0100

    added theme colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,17 +1,25 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+export const colors = {
+  white: '#FFFFFF',
+  midnight: '#00081D',
+  blue: '#005AE1',
+  lightcyan: '#D4FCFF',
+  darkcyan: '#34A6AE',
+};
+
 export default {
   light: {
-    text: '#000',
+    text: colors.midnight,
     background: '#fff',
     tint: tintColorLight,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
+    text: colors.lightcyan,
+    background: colors.midnight,
     tint: tintColorDark,
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
