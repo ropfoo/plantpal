d11ed80a0facee8a646a5cc98770abd0dc1b8d39
RopFoo <ropfoo@gmail.com>
2023-02-24 19:52:55 +0100

    plants hook with react query hooks

---
+++ hooks/usePlants.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { createPlant, deleteAllPlants, getPlants } from '../helper/plant';
+import { Plant } from '../helper/types';
+
+export function usePlants() {
+  const queryClient = useQueryClient();
+  const deleteAllPlantsMutation = useMutation(deleteAllPlants, {
+    mutationKey: ['plants'],
+    onMutate: () => {
+      queryClient.setQueriesData<Plant[]>(['plants'], () => []);
+    },
+  });
+  const createPlantMutation = useMutation(createPlant, {
+    mutationKey: ['plants'],
+    onMutate: (data) => {
+      queryClient.setQueryData<Plant[]>(['plants'], (plants) => [
+        ...(plants ?? []),
+        data,
+      ]);
+    },
+  });
+  const plantsQuery = useQuery(['plants'], getPlants);
+
+  return { plantsQuery, createPlantMutation, deleteAllPlantsMutation };
+}
