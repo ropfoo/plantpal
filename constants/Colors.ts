a5a7cf0e7b5d798db8207f7ad213e1f2472fe96c
RopFoo <ropfoo@gmail.com>
2023-02-25 11:10:23 +0100

    secondary background colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -4,11 +4,14 @@ const tintColorDark = '#fff';
 export const colors = {
   white: '#FFFFFF',
   midnight: '#00081D',
+  night: '#000E33',
+  day: '#E9FDFF',
   blue: '#005AE1',
   lightblue: '#3A85F6',
   bluegray: '#D7E2F3',
   darkbluegray: '#3A4A63',
   red: '#EC007F',
+  cyan: '#00E0FF',
   lightcyan: '#D4FCFF',
   darkcyan: '#34A6AE',
 };
@@ -21,6 +24,7 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
     disabled: colors.bluegray,
+    backgroundSecondary: colors.day,
   },
   dark: {
     text: colors.lightcyan,
@@ -29,5 +33,6 @@ export default {
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
     disabled: colors.darkbluegray,
+    backgroundSecondary: colors.night,
   },
 };
